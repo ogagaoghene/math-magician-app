4f1db03d45767d504733dea72d63892d5f10c66d
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-08-02 19:27:00 +0100

    Modified index.js file, index.html file, and deleted manifest.json, App.css, App.js files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends Component {
+  render() {
+    return (
+      <h1>Hello, World!</h1>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
